f46499ffbf4d126448ab4a000e12572d342f5abd
Fabio Lettieri <fabiolettieri9@gmail.com>
2020-11-18 14:56:12 -0300

    Estado e Cidade

---
+++ src/main/java/com/nelioalves/cursomc/domain/Estado.java
@@ -0,0 +1,39 @@
+package com.nelioalves.cursomc.domain;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Entity
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Estado implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@EqualsAndHashCode.Include
+	private Integer id;
+	private String nome;
+
+	@OneToMany(mappedBy = "estado")
+	private List<Cidade> cidades = new ArrayList<>();
+
+	public Estado() {
+	}
+
+	public Estado(Integer id, String nome) {
+		this.id = id;
+		this.nome = nome;
+	}
+
+}
